d4ba8724b92d7c97acf6bff982a472489d5b93b3
superfer2002 <fporcel2002@gmail.com>
2022-05-11 20:12:04 +0200

    routes addition

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,62 @@ const routes = [
     path: '/index',
     name: 'index',
     component: () => import('../views/IndexView.vue')
-  }
+  },
+  {
+    path: '/create',
+    name: 'create',
+    component: () => import('../views/CreateEventView.vue')
+  },
+  {
+    path: '/delete',
+    name: 'delte',
+    component: () => import('../views/DeleteEventView.vue')
+  },
+  {
+    path: '/eventology',
+    name: 'eventology',
+    component: () => import('../views/EventologyView.vue')
+  },
+  {
+    path: '/events',
+    name: 'events',
+    component: () => import('../views/EventsView.vue')
+  },
+  {
+    path: '/event',
+    name: 'event',
+    component: () => import('../views/EventView.vue')
+  },
+  {
+    path: '/friendProfile',
+    name: 'friendProfile',
+    component: () => import('../views/FriendProfileView.vue')
+  },
+  {
+    path: '/friends',
+    name: 'friends',
+    component: () => import('../views/FriendsView.vue')
+  },
+  {
+    path: '/privacy',
+    name: 'privacy',
+    component: () => import('../views/PrivacyView.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import('../views/ProfileView.vue')
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: () => import('../views/SearchView.vue')
+  },
+  {
+    path: '/terms',
+    name: 'terms',
+    component: () => import('../views/TermsView.vue')
+  },
   
 ]
 
